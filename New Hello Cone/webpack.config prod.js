99bbee265d102e2a8934620ddc13f86ee2b1d324
IPALab-junsu <somadubel@gmail.com>
2022-12-12 21:14:49 +0900

    volume rendeirng 로컬에서 실행하기 위한 밑작업

--- New Hello Cone/webpack.config prod.js
+++ New Hello Cone/webpack.config prod.js
@@ -1,20 +1,14 @@
 // FIXME: webpack.config.js의 loader규칙이 잘못되어 volume rendering이 bundle이 안되는 중인 듯. loader 작성법 알아본 뒤에 vtk.js 튜토리얼에서 확인해보기
-// https://github.com/Kitware/vtk-js/issues/1003
 
-const path = require("path");
-var vtkRules = require("vtk.js/Utilities/config/dependency.js").webpack.core
-  .rules;
+const path = require("path"); // node.js에서 파일의 경로를 핸들링하도록 도와주는 기능.
 
 // export const entry = "./source/index.js";
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/VolumeRendering.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "VolumeRendering_bundle.js",
-  },
-  module: {
-    rules: [].concat(vtkRules),
   },
 };
 
@@ -22,7 +16,7 @@ module.exports = {
 
 // // export const entry = "./source/index.js";
 // module.exports = {
-//   mode: "development",
+//   mode: "production",
 //   entry: "./src/index.js",
 //   output: {
 //     path: path.resolve(__dirname, "dist"),
--- README.md
+++
@@ -1,1 +0,0 @@
-# vtkjs_practice
