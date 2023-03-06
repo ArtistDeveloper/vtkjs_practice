80d0f03d82dd54ca95f630603dbf357a2d4ac0c8
kNU_local <somadubel@gmail.com>
2023-03-06 17:42:54 +0900

    Linter setting, cone_colored.js 작성중

--- New Tutorial/Coloring Cone/src/cone_colored.js
+++ New Tutorial/Coloring Cone/src/cone_colored.js
@@ -1,23 +1,58 @@
 import "@kitware/vtk.js/Rendering/Profiles/Geometry";
-import "@kitware/vtk.js/Rendering/Misc/RenderingAPIs"
+import "@kitware/vtk.js/Rendering/Misc/RenderingAPIs";
 
+import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor"
+import vtkConeSource from "@kitware/vtk.js/Filters/Sources/ConeSource";
+import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
 import vtkRenderWindow from "@kitware/vtk.js/Rendering/Core/RenderWindow";
 import vtkRenderer from "@kitware/vtk.js/Rendering/Core/Renderer";
 import vtkRenderWindowInteractor from "@kitware/vtk.js/Rendering/Core/RenderWindowInteractor";
+import vtkInteractorStyleTrackballCamera from "@kitware/vtk.js/Interaction/Style/InteractorStyleTrackballCamera";
 
 
 function createRenderWindow(e1) {
-    const renderWindow = vtkRenderWindow.newInstance();
-    const renderer = vtkRenderer.newInstance();
+  const renderWindow = vtkRenderWindow.newInstance();
+  const renderer = vtkRenderer.newInstance();
 
-    renderWindow.addRenderer(renderer);
+  renderWindow.addRenderer(renderer);
 
-    // picks webgl or webgpu
-    const apiSpecificRenderWindow = renderWindow.newAPISpecificView();
-    apiSpecificRenderWindow.setContainer(e1);
-    renderWindow.addview(apiSpecificRenderWindow);
+  // picks webgl or webgpu
+  const apiSpecificRenderWindow = renderWindow.newAPISpecificView();
+  apiSpecificRenderWindow.setContainer(e1);
+  renderWindow.addview(apiSpecificRenderWindow);
 
-    // create interactor and bind
-    const interactor = vtkRenderWindowInteractor.newInstance();
+  // create interactor and bind
+  const interactor = vtkRenderWindowInteractor.newInstance();
+  interactor.setInteractorStyle(
+    vtkInteractorStyleTrackballCamera.newInstance()
+  );
+
+  interactor.setView(apiSpecificRenderWindow);
+  interactor.initialize();
+  interactor.bindEvents(e1);
+
+  apiSpecificRenderWindow.setSize(600, 600);
+
+  return {
+    renderWindow,
+    renderer,
+    interactor,
+    apiSpecificRenderWindow,
+  };
 }
 
+const components = createRenderWindow(document.getElementById("app"));
+const { renderer, renderWindow } = components;
+
+// Create a cone
+function createConePipeline() {
+    const coneSource = vtkConeSource.newInstance();
+    const actor = vtkActor.newInstance();
+    const mapper = vtkMapper.newInstance();
+
+    actor.setMapper(mapper);
+    mapper.setInputConnection(coneSource.getOutputPort());
+
+    renderer.addActor(actor);
+    return { coneSource, mapper, actor };
+}
